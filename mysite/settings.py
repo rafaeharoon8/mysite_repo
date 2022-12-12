42e7b705290e82de6e1d9f8a62ecfce080b3a1de
rafaeharoon8 <118612423+rafaeharoon8@users.noreply.github.com>
2022-12-12 14:13:12 +0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,10 +82,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite_db',
+        'USER': 'b509489e0f6388',
+        'PASSWORD': '2aeef047',
+        'HOST': 'us-cdbr-east-06.cleardb.net',
+        'PORT': '3299',
     }
 }
 
